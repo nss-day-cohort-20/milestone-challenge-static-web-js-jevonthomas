65df712229d428481694a8d0cf3404ffd95c3929
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-08 20:54:53 -0500

    I'm stuck at trying to figure out how to console log user inputs

--- main.js
+++ main.js
@@ -15,12 +15,24 @@ subButton[0].addEventListener("click", function() {
 	let specChar = document.getElementById("special_character").value;
 		for (var i = 0; i < specialChar.length; i++) {
 		 	if (number < 10 && specChar === specialChar[i]) {
+				let choices = preference(number, specChar);
 				console.log("Special Character", specChar);
 		 		console.log("Tree Height", number);
-		 	} else {
-		 		alert("Enter a number between 1 - 10, and enter a special character!");
-		 	}
+		 		break;
+		 	} 
 		};
 });
 
 
+function preference(height, character) {
+	let treeDescription = {};
+	treeDescription.height = height;
+	treeDescription.character = character;
+	console.log("preference", treeDescription);
+	return treeDescription;
+};
+
+
+function tree(treeDescription) {
+
+};
