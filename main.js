8fdb8403ae2a9a904f0e88e586120dc16aeb022f
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-08 21:43:44 -0500

    tried some more stuff, but accomplished nothing

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "~"];
 let subButton = document.getElementsByClassName("submit_button");
-console.log("Submit Button", subButton);
 
 
 
@@ -15,24 +14,23 @@ subButton[0].addEventListener("click", function() {
 	let specChar = document.getElementById("special_character").value;
 		for (var i = 0; i < specialChar.length; i++) {
 		 	if (number < 10 && specChar === specialChar[i]) {
-				let choices = preference(number, specChar);
-				console.log("Special Character", specChar);
-		 		console.log("Tree Height", number);
-		 		break;
+		 		return preference(number, specChar);
 		 	} 
 		};
 });
 
 
+
+//Takes user input and creates a single object with two key/value properties
 function preference(height, character) {
 	let treeDescription = {};
 	treeDescription.height = height;
 	treeDescription.character = character;
-	console.log("preference", treeDescription);
 	return treeDescription;
 };
 
-
 function tree(treeDescription) {
-
+	for (var i = 0; i <= treeDescription.height; i++) {
+		console.log(treeDescription.character);
+	}
 };
