020fe14691d142519570631cea57888e3b208549
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-16 14:09:49 -0500

    I built the tree function to console log a tree equal to the height of what the user input, with the number of special characters increasing by 2 after each console log, and the number of spaces decreasing by 1 after each interval, with the initial amount of spaces logged being equal to the user input height minus 1; making a christmas tree-like structure

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+//To be used in a function that prevents the user from submitting characters other than what is listed
 let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "~"];
-let subButton = document.getElementsByClassName("submit_button");
-
-//An empty string for the special characters to go into
-var buildingBlocks = "";
 
 
 //This function grabs the information from both textboxes 
 //stores those values into variables after the submit button is clicked
 //Calls the next function with the inputs as its arguments
+let subButton = document.getElementsByClassName("submit_button");
 subButton[0].addEventListener("click", function() {
 	let height = document.getElementById("tree_height").value;
 	let specChar = document.getElementById("special_character").value;
 	preference(height, specChar);
+});
+
+//This function allows for the user to press enter after typing into the textboxes
+window.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+    	let height = document.getElementById("tree_height").value;
+			let specChar = document.getElementById("special_character").value;
+			preference(height, specChar);
+    }
 });
 
 //This function takes the user inputs and turns them into a single object with two key/value properties
@@ -22,11 +30,27 @@ function preference(height, character) {
 	tree(treeDescription);
 };
 
+
+//The tree function that accepts a single argument with two key/value pairs
+//and builds a pine tree
 function tree(treeDescription) {
-	for (var i = 0; i <= treeDescription.height; i++) {
-		buildingBlocks += (treeDescription.character);
-		if (i === 0 || i % 2 === 0) {	
-		console.log(buildingBlocks);
-		};
+	//Allows storage of increased number of special characters
+	let buildingBlocks = treeDescription.character;
+	//Allows storage of decreased number of spaces 
+	let spaces = "";
+	//for loop that loops as many times as depicted by user input
+	for (let i = 0; i < treeDescription.height; i++) {
+		//Initially store in a variable a number of spaces equal to the height number minus 1
+		//Decrease the amount of spaces after each full function interval
+		for (let j = 0; j < treeDescription.height - i; j++) {
+			//logs buildingBlocks after each interval
+			spaces += " ";
+		}
+		console.log(spaces + buildingBlocks);
+		//increases buildingBlocks by 2 after each iteration
+		buildingBlocks += treeDescription.character + treeDescription.character;
+		//returns spaces to equal no spaces
+		spaces = "";
 	}
 };
+
